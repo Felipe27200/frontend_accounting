d0ecc7a4efb4349fc31ebece56811ed0e5bf736b
Felipe Zea <48108543+Felipe27200@users.noreply.github.com>
2024-07-03 13:52:00 -0500

    Logout and absolute path

--- src/app/common_components/nav-bar/nav-bar.component.ts
+++ src/app/common_components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+// import { LocalStorageService } from '../../services/local-storage.service';
+import { LocalStorageService } from '@services/local-storage.service';
+
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -8,6 +13,12 @@ import { MenuItem } from 'primeng/api';
 })
 export class NavBarComponent implements OnInit {
   items: MenuItem[] | undefined;
+
+  constructor (
+    private router: Router,
+    private route: ActivatedRoute,
+    private localStorageService: LocalStorageService
+  ) {}
 
   ngOnInit(): void {
     this.items = [
@@ -29,6 +40,26 @@ export class NavBarComponent implements OnInit {
               }
           ]
       },
+      {
+        label: 'Users',
+        icon: 'pi pi-user',
+        items: [
+          {
+            label: "Logout",
+            command: () => {
+              this.logout();
+            }
+          }
+        ],
+        style: {'margin-left': 'auto'}
+      }
     ]
+  }
+
+  logout ()
+  {
+    this.localStorageService.removeItem("Bearer-token");
+
+    this.router.navigate(["/login"]);
   }
 }
