5c481da513ee802007dd0d0705ab0cfd81d1e6cf
Juan Felipe  J.Z. Zea Sanchez <jzea@db-system.corp>
2025-02-06 15:20:32 -0500

    Correct error: bad set up of Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // ...
+  content: [
+    "./src/**/*.{html,ts}", // Your HTML and TypeScript files
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [require('tailwindcss-primeui')]
 };
 
