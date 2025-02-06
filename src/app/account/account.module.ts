6972daafc822723f58d54f786513dfee4f9926dc
Juan Felipe  J.Z. Zea Sanchez <jzea@db-system.corp>
2025-02-06 09:18:17 -0500

    Version 17 to Version 18

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AccountRoutingModule } from './account-routing.module';
@@ -23,36 +23,33 @@ import { CalendarModule } from 'primeng/calendar';
 import { CardModule } from 'primeng/card';
 import { PanelModule } from 'primeng/panel';
 
-@NgModule({
-  declarations: [
-    AccountListComponent,
-    AccountCenterComponent,
-    AccountTableComponent
-  ],
-  imports: [
-    CommonModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    CalendarModule,
-    PanelModule,
-    CardModule,
-    InputTextModule,
-    FloatLabelModule,
-    DropdownModule,
-    TableModule,
-    SharedComponentModule,
-    AccountRoutingModule,
-  ],
-  providers: [
-    /**
-     * It's necessary to provide the HttpClient
-     * and set up the Interceptors if it has one or more.
-     */
-    provideHttpClient(
-      withInterceptors([JwtInterceptor])
-    ),
-  ]
-})
+@NgModule({ 
+    declarations: [
+        AccountListComponent,
+        AccountCenterComponent,
+        AccountTableComponent
+    ], 
+    imports: [
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        CalendarModule,
+        PanelModule,
+        CardModule,
+        InputTextModule,
+        FloatLabelModule,
+        DropdownModule,
+        TableModule,
+        SharedComponentModule,
+        AccountRoutingModule
+    ], 
+    providers: [
+        /**
+         * It's necessary to provide the HttpClient
+         * and set up the Interceptors if it has one or more.
+         */
+        provideHttpClient(withInterceptors([JwtInterceptor])),
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AccountModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule  } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser'; 
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 // Components
 import { AppComponent } from './app.component';
@@ -23,30 +23,24 @@ import { MenubarModule } from 'primeng/menubar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    CardModule,
-    ButtonModule,
-    InputTextModule,
-    MenubarModule,
-    FormsModule,
-    ReactiveFormsModule,
-    SharedComponentModule,
-    AccountModule,
-    CategoryModule,
-    FinancialStatementModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [
-    provideHttpClient(),
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        CardModule,
+        ButtonModule,
+        InputTextModule,
+        MenubarModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SharedComponentModule,
+        AccountModule,
+        CategoryModule,
+        FinancialStatementModule,
+        AppRoutingModule], providers: [
+        provideHttpClient(),
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule { }
