0f5176a17b27e31a79abc1bc1fa04c5ae9a24d1f
Juan Felipe  J.Z. Zea Sanchez <jzea@db-system.corp>
2025-02-13 16:45:35 -0500

    Enable editable row and first attempt to update the account

--- src/app/account/account-table/account-table.component.ts
+++ src/app/account/account-table/account-table.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 
+import { DateFormatterService } from '@services/date-formatter.service';
+import { AccountService } from '@services/account.service';
+
 @Component({
     selector: 'app-account-table',
     templateUrl: './account-table.component.html',
@@ -13,12 +16,60 @@ export class AccountTableComponent
 
   clonedFinancialData: { [s: string]: any } = {};
 
+  constructor(
+    private dateFormatter: DateFormatterService,
+    private accountService: AccountService
+  ) {}
+
   getTypeCatalogue(category: any)
   {
     if (category.hasOwnProperty("accountCatalogue") && (category.accountCatalogue !== null && category.accountCatalogue !== undefined))
       return category.accountCatalogue.typeAccount;
     else
       return category.typeAccount;
+  }
+
+  onRowEditSave(financialData: any)
+  {
+    for (let category of this.categoryList)
+    {
+      if (category.id == financialData.categoryDTO.id)
+      {
+        financialData.categoryDTO.name = category.name;
+        break;
+      }
+    }
+
+    if (financialData.date == null)
+      return;
+
+    let date = '';
+
+    if (financialData.date instanceof Date)
+      date = this.dateFormatter.formatDate(financialData.date);
+    else
+      date = financialData.date;
+
+    financialData.date = date;
+
+    let formatData = {
+      date: date,
+      amount: financialData.amount,
+      isRecurring: false,
+      categoryId: financialData.categoryDTO.id,
+      financialStatementId: financialData.financialStatement.id
+    };
+
+    this.accountService
+      .editAccount(formatData, financialData.id)
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (error) => {
+          console.error(error);
+        }
+      });
   }
 
   onRowEditInit(financialData: any)
